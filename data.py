39caa99fb90968fac8bf655b3043313534ed34df
murari-gitgenius64 <167095434+murari-gitgenius64@users.noreply.github.com>
2026-07-30 07:13:22 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,72 @@
+# data.py
+
+flights = [
+
+    {
+        "flight_no": "AK101",
+        "departure": "Hyderabad",
+        "arrival": "Mumbai",
+        "time": "09:00 AM",
+        "fare": 4500
+    },
+
+    {
+        "flight_no": "AK102",
+        "departure": "Hyderabad",
+        "arrival": "Delhi",
+        "time": "11:30 AM",
+        "fare": 5200
+    },
+
+    {
+        "flight_no": "AK103",
+        "departure": "Hyderabad",
+        "arrival": "Bangalore",
+        "time": "06:00 PM",
+        "fare": 3500
+    },
+
+    {
+        "flight_no": "AK104",
+        "departure": "Mumbai",
+        "arrival": "Hyderabad",
+        "time": "10:30 AM",
+        "fare": 4200
+    },
+
+    {
+        "flight_no": "AK105",
+        "departure": "Delhi",
+        "arrival": "Hyderabad",
+        "time": "02:30 PM",
+        "fare": 5300
+    },
+
+    {
+        "flight_no": "AK106",
+        "departure": "Bangalore",
+        "arrival": "Hyderabad",
+        "time": "07:30 PM",
+        "fare": 3400
+    },
+
+    {
+        "flight_no": "AK107",
+        "departure": "Hyderabad",
+        "arrival": "Mumbai",
+        "time": "03:30 PM",
+        "fare": 4800
+    },
+
+    {
+        "flight_no": "AK108",
+        "departure": "Hyderabad",
+        "arrival": "Delhi",
+        "time": "08:00 PM",
+        "fare": 5700
+    }
+
+]
+
+# Stores booked tickets during program execution
+bookings = []
